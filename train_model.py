6c9f27c60fb07ac3263b004fd25f33fbd5bea467
Jaeyoon Lee <lee.jaeyoon.lee@gmail.com>
2021-12-23 01:47:15 +0000

    Update train_model.py & Add new model

--- train_model.py
+++ train_model.py
@@ -2,47 +2,34 @@ import numpy as np
 import os
 
 
+
+# increase amount of board (1 -> 12)
+def flip_rotate(board, data):
+    for board_to_rotate in [board, np.flipud(board), np.fliplr(board)]:
+        data.append(board_to_rotate)
+        for i in range(3):
+            data.append(np.rot90(board_to_rotate, i))
+
+
 def train_board(positions, x_data, y_data):
-    init_boards_x = []
-    init_boards_y = []
+    for i in range(1, len(positions)-1):
+        board_x = np.zeros((19,19))
+        board_y = np.zeros((19,19))
 
-    for i in range(len(positions)-1):
-        init_board_x = np.zeros((19,19))
-        init_board_y = np.zeros((19,19))
         for j in range(i+1):
-            init_board_x[positions[i][1]][positions[i][0]] = 1 if i%2 == 0 else -1
-        init_board_y[positions[i+1][1]][positions[i+1][0]] = 1 if i%2 == 0 else -1
+            board_x[positions[j][1]][positions[j][0]] = 1 if j%2 == 0 else -1
 
-        init_boards_x.append(init_board_x)
-        init_boards_y.append(init_board_y)
+        board_y[positions[i+1][1]][positions[i+1][0]] = 1
 
-
-    flip_boards_x = []
-    flip_boards_y = []
-    
-    for init_board in init_boards_x:
-        flip_boards_x.append(np.flipud(init_board))  # flip the board up and down
-        flip_boards_x.append(np.fliplr(init_board))  # flip the board left and right
-    for init_board in init_boards_y:
-        flip_boards_y.append(np.flipud(init_board))
-        flip_boards_y.append(np.fliplr(init_board))
+        flip_rotate(board_x, x_data)
+        flip_rotate(board_y, y_data)
 
 
-    for board_x in np.concatenate((init_boards_x, flip_boards_x), axis=0):
-        x_data.append(board_x)
-        for i in range(3):
-            x_data.append(np.rot90(board_x, i))
-    for board_y in np.concatenate((init_boards_y, flip_boards_y), axis=0):
-        y_data.append(board_y)
-        for i in range(3):
-            y_data.append(np.rot90(board_y, i))
-
-            
 def array_to_1D(array):
     for i in range(len(array)):
         array[i] = array[i].flatten()
 
-            
+
 X_train = []
 Y_train = []
 
@@ -51,32 +38,33 @@ DIR = os.path.join(current_path, 'data')
 data_count = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
 
 
-data = []
-
 for i in range(data_count):
     with open(os.path.join(DIR, '{}.txt'.format(i))) as file:
         lines = file.readlines()
         lines = [list(map(int, line.rstrip().split(','))) for line in lines]
         train_board(lines, X_train, Y_train)
-        array_to_1D(X_train)
-        array_to_1D(Y_train)
+array_to_1D(Y_train)
 
-# print(type(Y_train[-1]))
+# print(X_train[-1])
 
 
 import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(64, activation='tanh'),
-    tf.keras.layers.Dense(128, activation='tanh'),
-    tf.keras.layers.Dense(361, activation='sigmoid'),
+X_train = np.array(X_train).reshape(len(X_train), 19,19,1)
+model = models.Sequential([
+    layers.Conv2D(64, 7, padding='same', activation='relu', input_shape=(19, 19, 1)),
+    layers.Conv2D(1, 1, padding='same', activation=None),
+    layers.Reshape((361,)),
+    layers.Activation('softmax')
 ])
+model.summary()
+
 print(len(X_train), len(Y_train))
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(np.array(X_train), np.array(Y_train), epochs=10)
-
+model.fit(np.array(X_train), np.array(Y_train), epochs=30)
 
 model.save(os.path.join(current_path, 'gomoku({}).h5'.format(data_count)))
-
 
